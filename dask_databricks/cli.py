a6ff0d4ff5ca91c29b174386474a69b9454c48d4
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2023-11-07 10:14:47 +0100

    Ad running on Databricks check and rich logging for output (#6)

--- dask_databricks/cli.py
+++ dask_databricks/cli.py
@@ -1,8 +1,19 @@
 import click
+import logging
 import os
-import subprocess
-import time
 import socket
+import subprocess
+import sys
+import time
+
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("dask_databricks")
 
 @click.group(name="databricks")
 def main():
@@ -12,17 +23,22 @@ def main():
 def run():
     """Run Dask processes on a Databricks cluster."""
 
-    print("Setting up Dask on a Databricks cluster.")
+    log.info("Setting up Dask on a Databricks cluster.")
 
     DB_IS_DRIVER = os.getenv('DB_IS_DRIVER')
     DB_DRIVER_IP = os.getenv('DB_DRIVER_IP')
 
+    if DB_DRIVER_IP is None or DB_IS_DRIVER is None:
+        log.error("Unable to find expected environment variables DB_IS_DRIVER and DB_DRIVER_IP. "
+                   "Are you running this command on a Databricks multi-node cluster?")
+        sys.exit(1)
+
     if DB_IS_DRIVER == "TRUE":
-        print("This node is the Dask scheduler.")
+        log.info("This node is the Dask scheduler.")
         subprocess.Popen(["dask", "scheduler"])
     else:
-        print("This node is a Dask worker.")
-        print(f"Connecting to Dask scheduler at {DB_DRIVER_IP}:8786")
+        log.info("This node is a Dask worker.")
+        log.info(f"Connecting to Dask scheduler at {DB_DRIVER_IP}:8786")
         while True:
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,7 +46,7 @@ def run():
                 sock.close()
                 break
             except ConnectionRefusedError:
-                print("Scheduler not available yet. Waiting...")
+                log.info("Scheduler not available yet. Waiting...")
                 time.sleep(1)
         subprocess.Popen(["dask", "worker", f"tcp://{DB_DRIVER_IP}:8786"])
 
