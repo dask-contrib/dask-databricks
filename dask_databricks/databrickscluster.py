62e8d1bcbe65030311b22e66ffd98186ae1a60cc
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2023-11-08 11:15:32 +0100

    Add Precommit (#23)
    * Add precommit config
    
    * Format with black and run isort

--- dask_databricks/databrickscluster.py
+++ dask_databricks/databrickscluster.py
@@ -1,25 +1,30 @@
 import os
 import uuid
-
-from distributed.deploy.cluster import Cluster
-from distributed.core import rpc
 from typing import Optional
+
+from distributed.core import rpc
+from distributed.deploy.cluster import Cluster
 from tornado.ioloop import IOLoop
 
 # Databricks Notebooks injects the `spark` session variable
-if 'spark' not in globals():
+if "spark" not in globals():
     spark = None
+
 
 class DatabricksCluster(Cluster):
     """Connect to a Dask cluster deployed via databricks."""
 
-    def __init__(self,
+    def __init__(
+        self,
         loop: Optional[IOLoop] = None,
-        asynchronous: bool = False,):
+        asynchronous: bool = False,
+    ):
         self.spark_local_ip = os.getenv("SPARK_LOCAL_IP")
         if self.spark_local_ip is None:
-            raise KeyError("Unable to find expected environment variable SPARK_LOCAL_IP. "
-                           "Are you running this on a Databricks driver node?")
+            raise KeyError(
+                "Unable to find expected environment variable SPARK_LOCAL_IP. "
+                "Are you running this on a Databricks driver node?"
+            )
         try:
             name = spark.conf.get("spark.databricks.clusterUsageTags.clusterId")
         except AttributeError:
@@ -30,10 +35,10 @@ class DatabricksCluster(Cluster):
             self._loop_runner.start()
             self.sync(self._start)
 
-
     async def _start(self):
-        self.scheduler_comm = rpc(f'{self.spark_local_ip}:8786')
+        self.scheduler_comm = rpc(f"{self.spark_local_ip}:8786")
         await super()._start()
+
 
 def get_client():
     """Get a Dask client connected to a Databricks cluster."""
